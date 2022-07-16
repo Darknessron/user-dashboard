774a28135ec8d723e3f37046f1d8a376e0c21ff7
Ron <cg12192001@gmail.com>
2022-07-16 16:58:31 +0800

    Apply Auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import React from 'react';
+import {UserProvider} from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+/**
+ * Main
+ * @param {Object} Component
+ * @param {Object} pageProps
+ * @return {JSX.Element}
+ * @constructor
+ */
+export default function App({Component, pageProps}) {
+  return (
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+  );
 }
-
-export default MyApp
